d6c815f9de41ceae247df41b43ac99afb917f9a7
Jason Harris <jason.harris@sourcegraph.com>
2024-10-28 10:44:58 -0500

    backup

--- lua/hawk/plugin/nvim-tree.lua
+++ lua/hawk/plugin/nvim-tree.lua
@@ -19,13 +19,13 @@ vim.opt.termguicolors = true
 require("nvim-tree").setup({
     sort_by = "case_sensitive",
     view = {
-        width = 30,
+        width = 40,
     },
     renderer = {
         group_empty = true,
     },
     filters = {
-        dotfiles = false,
+        dotfiles = true,
     },
 })
 
